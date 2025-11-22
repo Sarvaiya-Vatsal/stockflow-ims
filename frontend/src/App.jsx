af4ced9b6d983268734aac728e44be0353ad551d
Kush-limbasiya <freefireplayer623@gmail.com>
2025-11-22 12:45:37 +0530

    Create React frontend with Tailwind styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-semibold text-gray-800">StockFlow IMS</h1>
+    </div>
+  );
+}
+
+export default App;
+
